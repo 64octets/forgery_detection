6a2545fd0dd2a7271c75ca8135874304f89ddac4
teerakanok <songpon.te@cysec.cs.ritsumei.ac.jp>
2015-03-09 06:11:34 +0900

    Fix CrossFeatureTest + Add some resources

--- Matlab/CrossFeatureTest.m
+++ Matlab/CrossFeatureTest.m
@@ -30,26 +30,52 @@ function output = CrossFeatureTest (img_path, resaved_img_path, block_size, q_mu
          
          %V.   construct vector of results
          %vect = [nmap(i,j) kmap(i,j) 0];
-         vect = [imap(i,j) qmap(i,j) 0];
+         vect = [qmap(i,j) imap(i,j) 0];
+         %vect = [imap(i,j) qmap(i,j) 0];
          
          %VI.  do cross product
-         out_vect = cross(vect, Principal_vect);
+         %out_vect = cross(vect, Principal_vect);
+         out_vect = cross(Principal_vect, vect);
          output(i,j) = out_vect(1,3);
         end
     end
-    output_vector = output(:);
+    %adjust value range of the output
+    output = abs(output);
+    min_val = min(output(:));
+    max_val = max(output(:));
+    
+    %for selecting top 80% value
+    threshold_percent = 0.8;
+    
+    range = max_val - min_val;
+    
+    for i = 1:w
+        for j = 1:h
+            output(i,j) = ((max_val - output(i,j)) / range);
+            %{
+            if output(i,j) > 0.9
+                output(i, j) = 0;
+            else
+            %}    
+            if output(i, j) < 0.3
+                output(i,j) = 1;
+            end
+        end
+    end
+    
+    %output_vector = output(:);
     
     %malfunction due to NaN value within the output matrix
     %figure, plot(FFT_MAT(output));
     
     %figure, bar3(output);
-    %figure, surf(output);
+    figure, surf(output);
     %figure, plot(output_vector); % output in sequence signal
     figure('name','Proposed Algorithm'); displayMatrixInColorImage(output);
     %figure, imshow(output);
     %figure, displayMatrixInColorImage(nmap);
     %figure, displayMatrixInColorImage(kmap);
-    figure('name','Quantization Noise'); imshow(rgb2gray(qmapImg));
+    figure('name','Quantization Noise'); imshow(qmapImg);
     figure('name','Re-Interpolation'); imshow(imapImg);
     %figure, displayMatrixInColorImage(imapImg);
 end
